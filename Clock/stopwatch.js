3054faee152b75b9f8dfd3ede55be820c762875b
mhtuhin <mahabubhasantuhin2005@gmail.com>
2024-12-01 20:59:59 +0600

    Added some project that do

---
+++ Clock/stopwatch.js
@@ -0,0 +1,53 @@
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let msec = 00;
+let secs = 00;
+let mins = 00;
+let hours = 00;
+
+let timerId = null;
+
+startBtn.addEventListener('click',function(){
+    if (timerId !== null) {
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click',function(){
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00 : 00`;
+    msec = secs = mins = hours = 00;
+});
+
+function startTimer(){
+    msec++;
+    if (msec == 100) {
+        msec = 0;
+        secs++;
+        if (secs == 60) {
+            secs = 0;
+            mins++;
+            if (mins == 60) {
+                mins = 0;
+                hours++;
+                if (hours == 24) {
+                    clearInterval(timerId);  
+                }
+            }
+        }
+    }
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+    let hoursString = hours < 10 ? `0${hours}` : hours;
+
+    timerDisplay.innerHTML = `${hoursString} : ${minsString} : ${secsString} : ${msecString}`;
+}
